f59fd72bee79797a1c3ad7a330ce858196bd4cb9
misasan1612@gmail.com <misasan1612@gmail.com>
2017-11-28 15:21:29 +0700

    cannot pull

--- app/src/main/java/com/example/davidbezalellaoli/lastmobilemikroskil/modules/home/activities/Home.java
+++ app/src/main/java/com/example/davidbezalellaoli/lastmobilemikroskil/modules/home/activities/Home.java
@@ -3,28 +3,33 @@ package com.example.davidbezalellaoli.lastmobilemikroskil.modules.home.activitie
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.davidbezalellaoli.lastmobilemikroskil.R;
+import com.example.davidbezalellaoli.lastmobilemikroskil.modules.home.fragments.Dashboard;
+import com.example.davidbezalellaoli.lastmobilemikroskil.modules.home.fragments.List;
 
 public class Home extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction =fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    transaction.replace(R.id.content,new List()).commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    transaction.replace(R.id.content,new Dashboard()).commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -37,9 +42,11 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction =fragmentManager.beginTransaction();
+        transaction.replace(R.id.content,new List()).commit();
     }
 
 }
