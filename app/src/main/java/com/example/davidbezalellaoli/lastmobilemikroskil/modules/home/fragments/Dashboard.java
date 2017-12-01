5ccb3728d1059bdc53d3e3b1c89683c9be1ce11a
Silfi Langie <silfilangie04@gmail.com>
2017-12-01 17:29:41 +0700

    Melengkapi informasi dan gambar pada tab Home & membuat RV grid pada tab Dashboard

--- app/src/main/java/com/example/davidbezalellaoli/lastmobilemikroskil/modules/home/fragments/Dashboard.java
+++ app/src/main/java/com/example/davidbezalellaoli/lastmobilemikroskil/modules/home/fragments/Dashboard.java
@@ -1,6 +1,5 @@
 package com.example.davidbezalellaoli.lastmobilemikroskil.modules.home.fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -15,24 +14,25 @@ import com.example.davidbezalellaoli.lastmobilemikroskil.R;
 import com.example.davidbezalellaoli.lastmobilemikroskil.SplashScreen;
 import com.example.davidbezalellaoli.lastmobilemikroskil.modules.auth.activities.AuthParent;
 import com.example.davidbezalellaoli.lastmobilemikroskil.modules.home.adapters.User_RV_Adapter;
+import com.example.davidbezalellaoli.lastmobilemikroskil.modules.home.adapters.User_RV_Adapter_Grid;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Home extends Fragment {
+public class Dashboard extends Fragment {
 
     private View view;
     private RecyclerView rv;
-    private User_RV_Adapter userRvAdapter;
+    private User_RV_Adapter_Grid userRvAdapterGrid;
 
-    public Home() {
+    public Dashboard() {
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
         initView();
         initObject();
@@ -41,23 +41,21 @@ public class Home extends Fragment {
     }
 
     private void initView() {
-        rv = (RecyclerView) view.findViewById(R.id.home_rv);
-
-        /*LayoutManager: linear*/
-        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv = (RecyclerView) view.findViewById(R.id.dashboard_rv);
 
         /*LayoutManager: Grid*/
-//        rv.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        rv.setLayoutManager(new GridLayoutManager(getContext(), 2));
         rv.setHasFixedSize(true);
     }
 
     private void initObject() {
-        userRvAdapter = new User_RV_Adapter();
+        userRvAdapterGrid = new User_RV_Adapter_Grid();
 
         /*setting user rv adapter: inject data to adapter*/
-        userRvAdapter.setUsers(SplashScreen.users);
+        userRvAdapterGrid.setUsers(SplashScreen.users);
 
-        rv.setAdapter(userRvAdapter);
+        rv.setAdapter(userRvAdapterGrid);
     }
 
 }
+
